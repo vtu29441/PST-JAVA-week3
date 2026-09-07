2e0b021b905e4097f42ccc40dbcc0e758f57022a
vtu29441 <vtu29441@veltech.edu.in>
2026-09-07 21:59:14 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 8100-java-exception-handling-Hackerrank/8100-java-exception-handling.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.time.*;
+
+class Result {
+
+    public static String findDay(int month, int day, int year) {
+        LocalDate date = LocalDate.of(year, month, day);
+        return date.getDayOfWeek().toString();
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+
+        String[] input = br.readLine().split(" ");
+
+        int month = Integer.parseInt(input[0]);
+        int day = Integer.parseInt(input[1]);
+        int year = Integer.parseInt(input[2]);
+
+        System.out.println(Result.findDay(month, day, year));
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
