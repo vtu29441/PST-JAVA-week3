7549009b242c6a2200437c569c8ec45d4b047d22
vtu29441 <vtu29441@veltech.edu.in>
2026-09-07 21:49:51 +0530

    Force committed solution - Time: O(n), Space: O(1)

---
+++ 8712-java-interface-Hackerrank/8712-java-interface.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+interface AdvancedArithmetic {
+    int divisor_sum(int n);
+}
+
+class MyCalculator implements AdvancedArithmetic {
+
+    public int divisor_sum(int n) {
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        MyCalculator myCalculator = new MyCalculator();
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println("I implemented: AdvancedArithmetic");
+        System.out.println(myCalculator.divisor_sum(n));
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
